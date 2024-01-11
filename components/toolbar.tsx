76f204fcf64470ba2cdd5bdf9213e2ea174f5fdc
Nitesh000 <ntudu040@gmail.com>
2024-01-11 13:27:03 +0530

    feat: done with the icon setup

---
+++ components/toolbar.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile, X } from "lucide-react";
+import TextareaAutosize from "react-textarea-autosize";
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+  const removeIcon = useMutation(api.documents.removeIcon);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
+  const onRemoveIcon = () => {
+    removeIcon({
+      id: initialData._id,
+    });
+  };
+
+  return (
+    <div className="relative pl-[54px] group">
+      {!!initialData.icon && !preview && (
+        <div className="flex gap-x-2 items-center pt-6 group/icon">
+          <IconPicker onChange={onIconSelect}>
+            <p className="text-6xl hover:opacity-75">{initialData.icon}</p>
+          </IconPicker>
+          <Button
+            className="text-xs rounded-full opacity-0 transition text-muted-foreground group-hover/icon:opacity-100"
+            variant="outline"
+            onClick={onRemoveIcon}
+            size="icon"
+          >
+            <X className="w-4 h-4" />
+          </Button>
+        </div>
+      )}
+      {!!initialData.icon && preview && (
+        <p className="pt-6 text-6xl">{initialData.icon}</p>
+      )}
+
+      <div className="flex gap-x-1 py-4 opacity-0 group-hover:opacity-100 itemx-center">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={onIconSelect}>
+            <Button
+              className="text-xs text-muted-foreground"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="mr-2 w-4 h-4" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-xs text-muted-foreground"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="mr-2 w-4 h-4" />
+            Add cover
+          </Button>
+        )}
+      </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dar:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.4px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dar:text-[#CFCFCF]"
+        >
+          {initialData.title}
+        </div>
+      )}
+    </div>
+  );
+};
